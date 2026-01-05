9f6e992d47f6347b1e77ba6d029252aebd7ded03
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2026-01-05 17:59:20 -0400

    feat: created new package downloader

---
+++ internal/downloader/downloader.go
@@ -0,0 +1,45 @@
+package downloader
+
+import (
+	"cristhianflo/vid-dl/internal/input"
+	"errors"
+)
+
+type FileExtension string
+
+const (
+	Mhtml FileExtension = "mhtml"
+	M4a   FileExtension = "m4a"
+	Webm  FileExtension = "webm"
+	Mp3   FileExtension = "mp3"
+	Mp4   FileExtension = "mp4"
+)
+
+type Video struct {
+	ID      string
+	Title   string
+	Formats []Format
+}
+
+type Format struct {
+	ID         string
+	Ext        FileExtension
+	Resolution string
+	Fps        float32
+	Filesize   int64
+}
+
+type Downloader interface {
+	GetFormats() (*Video, error)
+}
+
+func NewDownloader(source *input.VideoSource) (Downloader, error) {
+	switch source.Type {
+	case input.YoutubeVideo:
+		return &YtdlpDownloader{
+			source: source,
+		}, nil
+	default:
+		return nil, errors.New("unsupported video source")
+	}
+}
